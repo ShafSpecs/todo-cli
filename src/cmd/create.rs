70d6ec5bcec58b433cb1a5f98213dac08393ed23
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-02-05 15:46:00 +0100

    refcatored file namings

--- src/cmd/create.rs
+++ src/cmd/create.rs
@@ -1,5 +1,8 @@
 use crate::structs::todo;
+use crate::db::write;
 
-pub fn add_todo(todo_content: String) {
+pub fn create_todo(todo_content: String) {
   let new_todo = todo::Todo::new(&todo_content);
+
+  write::write_to_db(new_todo);
 }
