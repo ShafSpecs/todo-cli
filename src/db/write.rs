27394157ecd2e81fc45617f27391081dc31b9795
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-02-13 12:14:44 +0100

    Smoothing out writing function

--- src/db/write.rs
+++ src/db/write.rs
@@ -1,7 +1,33 @@
-use crate::structs::todo::Todo;
+use serde_json::{Value, json};
+use crate::{structs::{todo::Todo, collection::Collection}, db::read};
 use std::fs::write;
 
-pub fn write_to_db(todo: Todo) -> () {
-    write("./db.json", Todo::get_todo(todo).as_bytes()).unwrap();
+pub fn write_to_db(todo: Todo, category: Option<String>) -> () {
+    let current_db_content = read::read_db();
+
+    if current_db_content.to_string() == "{}" {
+        write("./db.json", init_db().to_string()).unwrap();
+    }
+
+    if category.unwrap_or(String::from("Uncategorized")) == "Uncategorized" {
+
+        // write("./db.json", new_db_content.to_string()).unwrap();
+    }
+
+
+    // write("./db.json", Todo::get_todo(todo).as_bytes()).unwrap();
     ()
+}
+
+fn init_db() -> Value {
+    return json!({
+        "name": "todo-cli",
+        "categories": [
+            {
+                "key": "1",
+                "name": "Uncategorized",
+                "todos": []
+            }
+        ]
+    })
 }
