16073e1a7b0687dac97f2f4fc158a0b614355f8f
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-02-13 12:14:13 +0100

    Working on the create :yawning_face:

--- src/cmd/create.rs
+++ src/cmd/create.rs
@@ -4,5 +4,6 @@ use crate::db::write;
 pub fn create_todo(todo_content: String) {
   let new_todo = todo::Todo::new(&todo_content);
 
-  write::write_to_db(new_todo);
+  todo!("Fix this guy");
+  // write::write_to_db(new_todo, );
 }
